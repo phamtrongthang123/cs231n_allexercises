9ce5abd9b04048c4bc8366a8ce0d456064107293
Trong Thang <phamtrongthang123@gmail.com>
2019-08-06 11:09:25 +0700

    update script

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 sudo apt install vim
 cd /content/cs231n_allexercises/assignment3/cs231n/datasets
+chmod +x get_assignment3_data
 ./get_assignment3_data.sh
-cd /content/cs231n_allexercises/assignment3/cs231n/datasets
+cd /content/cs231n_allexercises/assignment2/cs231n/datasets
+chmod +x get_datasets.sh
 ./get_datasets.sh
-cd /content/cs231n_allexercises/assignment3/cs231n/datasets
+cd /content/cs231n_allexercises/assignment1/cs231n/datasets
+chmod +x get_datasets.sh
 ./get_datasets.sh
