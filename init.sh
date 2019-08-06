b97ac99bbb5bb1f0b1da4e579aa5fcf6f9dfa420
Trong Thang <phamtrongthang123@gmail.com>
2019-08-06 11:06:46 +0700

    add download auto

---
+++ init.sh
@@ -0,0 +1,6 @@
+cd /content/cs231n_allexercises/assignment3/cs231n/datasets
+./get_assignment3_data.sh
+cd /content/cs231n_allexercises/assignment3/cs231n/datasets
+./get_datasets.sh
+cd /content/cs231n_allexercises/assignment3/cs231n/datasets
+./get_datasets.sh
